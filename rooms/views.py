c5bd3040846bfecbb59fe066a108b15ba7022ef6
siheon <korimse@gmail.com>
2021-01-20 23:44:05 +0900

    airbnb

--- rooms/views.py
+++ rooms/views.py
@@ -1,6 +1,20 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.views.generic import ListView, DetailView
+from . import models
 
 
-def all_rooms(request):
-    return render(request, "all_rooms.html")
+class HomeView(ListView):
+
+    """ HomeView Definition """
+
+    model = models.Room
+    paginate_by = 10
+    paginate_orphans = 5
+    ordering = "created"
+    context_object_name = "rooms"
+
+
+class RoomDetail(DetailView):
+
+    """ RoomDetail Definition """
+
+    model = models.Room
